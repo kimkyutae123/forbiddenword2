9f5b243b736a57b86e4346c66a336d50b9c4dbd6
kyutae_kim <kendm1@naver.com>
2025-12-30 17:24:03 +0900

    12.30 금칙어게임

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ const PORT = process.env.PORT || 4000;
 
 const io = new Server(server, {
     cors: {
-       origin:[ "http://localhost:3000",
-         "https://forbiddenword2.onrender.com"
-           ],
+        origin: [ "https://forbiddenword2.vercel.app",
+        "https://localhost:3000"
+        ],
         methods: ["GET", "POST"],
 
     },
